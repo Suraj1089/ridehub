21589abbda2211d4019ec208ac43f03a742783f0
Suraj Pisal <surajpisal113@gmail.com>
2024-09-03 22:41:18 +0530

    add gitnore , env for secret management, and function to list issues.

--- main.go
+++ main.go
@@ -3,29 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/google/go-github/v64/github"
-	"github.com/spf13/viper"
+	"github.com/joho/godotenv"
 )
-
-type Config struct {
-	Token string `mapstructure:"Token"`
-}
-
-func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
-	viper.AutomaticEnv()
-
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
-	}
-
-	err = viper.Unmarshal(&config)
-	return
-}
 
 // ""
 type RiderPull struct {
@@ -36,13 +18,28 @@ type RiderPull struct {
 	URL    string   `json:"url,omitempty"`
 }
 
-func RideHubClient() *github.Client {
-	config, _ := LoadConfig(".")
-	return github.NewClient(nil).WithAuthToken(config.Token)
+type RiderIssue struct {
+	ID               int64                    `json:"id,omitempty"`
+	State            string                   `json:"state,omitempty"`
+	Title            string                   `json:"title,omitempty"`
+	Labels           []*string                `json:"labels,omitempty"`
+	URL              string                   `json:"url,omitempty"`
+	PullRequestLinks *github.PullRequestLinks `json:"pull_request,omitempty"`
 }
 
-func ListPullRequests(ctx context.Context, owner string, repoName string, options *github.PullRequestListOptions) ([]RiderPull, error) {
-	rider := RideHubClient()
+type RiderClient struct {
+	Issues       *github.IssuesService
+	PullRequests *github.PullRequestsService
+	Users        *github.UsersService
+	Teams        *github.TeamsService
+}
+
+func GetRiderClient() *RiderClient {
+	client := github.NewClient(nil).WithAuthToken(os.Getenv("GITHUB_SECRET_TOKEN"))
+	return &RiderClient{Issues: client.Issues, PullRequests: client.PullRequests, Users: client.Users, Teams: client.Teams}
+}
+
+func (rider *RiderClient) RiderPullRequests(ctx context.Context, owner string, repoName string, options *github.PullRequestListOptions) ([]RiderPull, error) {
 	pulls, _, err := rider.PullRequests.List(ctx, owner, repoName, options)
 	if err != nil {
 		panic(err)
@@ -64,18 +61,51 @@ func ListPullRequests(ctx context.Context, owner string, repoName string, option
 		riderPulls = append(riderPulls, riderPull)
 	}
 	return riderPulls, err
+}
+
+func (rider *RiderClient) RiderIssues(ctx context.Context, owner string, repoName string, options *github.IssueListOptions) ([]RiderIssue, error) {
+	issues, _, err := rider.Issues.List(ctx, false, options)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var riderIssues []RiderIssue
+	// output format id : Title : Url : state : labels
+	for _, val := range issues {
+		var labels []*string
+		for _, label := range val.Labels {
+			labels = append(labels, (label.Name))
+		}
+		riderIssue := RiderIssue{
+			ID:               *val.ID,
+			State:            *val.State,
+			Title:            *val.Title,
+			Labels:           labels,
+			URL:              *val.URL,
+			PullRequestLinks: val.PullRequestLinks,
+		}
+		riderIssues = append(riderIssues, riderIssue)
+	}
+	return riderIssues, err
 
 }
 
 func main() {
 	fmt.Println("Welcome to ridehub")
 	ctx := context.Background()
-	pullOptions := &github.PullRequestListOptions{State: "open"}
-	pulls, err := ListPullRequests(ctx, "Suraj1089", "SPPU-Result-Convertor", pullOptions)
+	err := godotenv.Load()
 	if err != nil {
 		fmt.Println(err)
 	}
-	for _, pull := range pulls {
-		fmt.Println(pull)
+	rider := GetRiderClient()
+	pullOptions := &github.PullRequestListOptions{State: "open"}
+	pulls, err := rider.RiderPullRequests(ctx, "Suraj1089", "SPPU-Result-Convertor", pullOptions)
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Println(pulls)
+	fmt.Println("Issues")
+	opts := &github.IssueListOptions{Filter: "created"}
+	issues, err := rider.RiderIssues(ctx, "Suraj1089", "SPPU-Result-Convertor", opts)
+	fmt.Println(issues)
+
 }
