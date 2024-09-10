350d58ade9fc253824697bb130c4ffd8e48996aa
Suraj Pisal <surajpisal113@gmail.com>
2024-09-10 08:17:29 +0530

    file modularization. add functions to getpull, getlabel, addlabels

--- main.go
+++ main.go
@@ -3,109 +3,25 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
+	"strings"
 
-	"github.com/google/go-github/v64/github"
+	ridehub "github.com/Suraj1089/ridehub/pkg"
 	"github.com/joho/godotenv"
 )
 
-// ""
-type RiderPull struct {
-	ID     int64    `json:"id,omitempty"`
-	State  string   `json:"state,omitempty"`
-	Title  string   `json:"title,omitempty"`
-	Labels []string `json:"labels,omitempty"`
-	URL    string   `json:"url,omitempty"`
-}
-
-type RiderIssue struct {
-	ID               int64                    `json:"id,omitempty"`
-	State            string                   `json:"state,omitempty"`
-	Title            string                   `json:"title,omitempty"`
-	Labels           []*string                `json:"labels,omitempty"`
-	URL              string                   `json:"url,omitempty"`
-	PullRequestLinks *github.PullRequestLinks `json:"pull_request,omitempty"`
-}
-
-type RiderClient struct {
-	Issues       *github.IssuesService
-	PullRequests *github.PullRequestsService
-	Users        *github.UsersService
-	Teams        *github.TeamsService
-}
-
-func GetRiderClient() *RiderClient {
-	client := github.NewClient(nil).WithAuthToken(os.Getenv("GITHUB_SECRET_TOKEN"))
-	return &RiderClient{Issues: client.Issues, PullRequests: client.PullRequests, Users: client.Users, Teams: client.Teams}
-}
-
-func (rider *RiderClient) RiderPullRequests(ctx context.Context, owner string, repoName string, options *github.PullRequestListOptions) ([]RiderPull, error) {
-	pulls, _, err := rider.PullRequests.List(ctx, owner, repoName, options)
-	if err != nil {
-		panic(err)
-	}
-	var riderPulls []RiderPull
-	// output format id : Title : Url : state : labels
-	for _, val := range pulls {
-		var labels []string
-		for _, label := range val.Labels {
-			labels = append(labels, *label.Name)
-		}
-		riderPull := RiderPull{
-			ID:     *val.ID,
-			State:  *val.State,
-			Title:  *val.Title,
-			Labels: labels,
-			URL:    *val.URL,
-		}
-		riderPulls = append(riderPulls, riderPull)
-	}
-	return riderPulls, err
-}
-
-func (rider *RiderClient) RiderIssues(ctx context.Context, owner string, repoName string, options *github.IssueListOptions) ([]RiderIssue, error) {
-	issues, _, err := rider.Issues.List(ctx, false, options)
-	if err != nil {
-		fmt.Println(err)
-	}
-	var riderIssues []RiderIssue
-	// output format id : Title : Url : state : labels
-	for _, val := range issues {
-		var labels []*string
-		for _, label := range val.Labels {
-			labels = append(labels, (label.Name))
-		}
-		riderIssue := RiderIssue{
-			ID:               *val.ID,
-			State:            *val.State,
-			Title:            *val.Title,
-			Labels:           labels,
-			URL:              *val.URL,
-			PullRequestLinks: val.PullRequestLinks,
-		}
-		riderIssues = append(riderIssues, riderIssue)
-	}
-	return riderIssues, err
-
-}
 
 func main() {
-	fmt.Println("Welcome to ridehub")
 	ctx := context.Background()
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println(err)
 	}
-	rider := GetRiderClient()
-	pullOptions := &github.PullRequestListOptions{State: "open"}
-	pulls, err := rider.RiderPullRequests(ctx, "Suraj1089", "SPPU-Result-Convertor", pullOptions)
+
+	rider := ridehub.GetRiderClient()
+
+	pulls, err := rider.GetPullLabels(ctx, "Suraj1089", "SPPU-RESULT-CONVERTER", 24)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(strings.Split(err.Error(), ":")[2])
 	}
 	fmt.Println(pulls)
-	fmt.Println("Issues")
-	opts := &github.IssueListOptions{Filter: "created"}
-	issues, err := rider.RiderIssues(ctx, "Suraj1089", "SPPU-Result-Convertor", opts)
-	fmt.Println(issues)
-
 }
--- pkg/ridehub.go
+++ pkg/ridehub.go
@@ -1,19 +1,127 @@
 package ridehub
 
 import (
-	"fmt"
+	"context"
+	"os"
 
 	"github.com/google/go-github/v64/github"
 )
 
-const Token string = "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"
-
-func Ridehub() *github.Client {
-	client := github.NewClient(nil).WithAuthToken(Token)
-	return client
+// ""
+type RiderPull struct {
+	ID     int64    `json:"id,omitempty"`
+	State  string   `json:"state,omitempty"`
+	Title  string   `json:"title,omitempty"`
+	Labels []string `json:"labels,omitempty"`
+	URL    string   `json:"url,omitempty"`
 }
 
-func ListPr(username string) {
-	client := github.NewClient(nil).WithAuthToken(Token)
-	fmt.Println(client.PullRequests)
+type RiderIssue struct {
+	ID               int64                    `json:"id,omitempty"`
+	State            string                   `json:"state,omitempty"`
+	Title            string                   `json:"title,omitempty"`
+	Labels           []*string                `json:"labels,omitempty"`
+	URL              string                   `json:"url,omitempty"`
+	PullRequestLinks *github.PullRequestLinks `json:"pull_request,omitempty"`
+}
+
+type RiderClient struct {
+	Issues       *github.IssuesService
+	PullRequests *github.PullRequestsService
+	Users        *github.UsersService
+	Teams        *github.TeamsService
+}
+
+func GetRiderClient() *RiderClient {
+	client := github.NewClient(nil).WithAuthToken(os.Getenv("GITHUB_SECRET_TOKEN"))
+	return &RiderClient{Issues: client.Issues, PullRequests: client.PullRequests, Users: client.Users, Teams: client.Teams}
+}
+
+func (rider *RiderClient) RiderPullRequests(ctx context.Context, owner string, repoName string, options *github.PullRequestListOptions) ([]RiderPull, error) {
+	pulls, _, err := rider.PullRequests.List(ctx, owner, repoName, options)
+	if err != nil {
+		return nil, err
+	}
+	var riderPulls []RiderPull
+	// output format id : Title : Url : state : labels
+	for _, val := range pulls {
+		var labels []string
+		for _, label := range val.Labels {
+			labels = append(labels, *label.Name)
+		}
+		riderPull := RiderPull{
+			ID:     *val.ID,
+			State:  *val.State,
+			Title:  *val.Title,
+			Labels: labels,
+			URL:    *val.URL,
+		}
+		riderPulls = append(riderPulls, riderPull)
+	}
+	return riderPulls, err
+}
+
+func (rider *RiderClient) RiderIssues(ctx context.Context, owner string, repoName string, options *github.IssueListOptions) ([]RiderIssue, error) {
+	issues, _, err := rider.Issues.List(ctx, false, options)
+	if err != nil {
+		return nil, err
+	}
+	var riderIssues []RiderIssue
+	// output format id : Title : Url : state : labels
+	for _, val := range issues {
+		var labels []*string
+		for _, label := range val.Labels {
+			labels = append(labels, (label.Name))
+		}
+		riderIssue := RiderIssue{
+			ID:               *val.ID,
+			State:            *val.State,
+			Title:            *val.Title,
+			Labels:           labels,
+			URL:              *val.URL,
+			PullRequestLinks: val.PullRequestLinks,
+		}
+		riderIssues = append(riderIssues, riderIssue)
+	}
+	return riderIssues, err
+}
+
+func (rider *RiderClient) AddLabelToPull(ctx context.Context, owner string, repoName string, pullNumber int, labelsToAdd []string) ([]string, error) {
+	_, _, err := rider.PullRequests.Get(ctx, owner, repoName, pullNumber)
+	if err != nil {
+		return nil, err
+	}
+	labels, _, err := rider.Issues.AddLabelsToIssue(ctx, owner, repoName, pullNumber, labelsToAdd)
+	if err != nil {
+		return nil, err
+	}
+	var allLabelsInPull []string
+	for _, val := range labels {
+		allLabelsInPull = append(allLabelsInPull, *val.Name)
+	}
+	return allLabelsInPull, nil
+}
+
+func (rider *RiderClient) GetPullLabels(ctx context.Context, owner string, repoName string, pullNumber int) ([]string, error) {
+	pull, _, err := rider.PullRequests.Get(ctx, owner, repoName, pullNumber)
+	if err != nil {
+		return nil, err
+	}
+	var labels []string
+	for _, val := range pull.Labels {
+		labels = append(labels, *val.Name)
+	}
+	return labels, nil
+}
+
+func (rider *RiderClient) RemoveLabelFromPull(ctx context.Context, owner string, repoName string, pullNumber int, label string) ([]string, error) {
+	_, err := rider.Issues.RemoveLabelForIssue(ctx, owner, repoName, pullNumber, label)
+	if err != nil {
+		return nil, err
+	}
+	labels, err := rider.GetPullLabels(ctx, owner, repoName, pullNumber)
+	if err != nil {
+		return nil, err
+	}
+	return labels, nil
 }
